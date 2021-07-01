e359457aa9ab2e17fdb2ec6c19261af43ae65619
Steven Athouel <sathouel@gmail.com>
2021-07-01 18:14:02 +0300

    bug fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mirakl-api-client",
-    version="0.0.1",
+    version="0.0.2",
     author="Steven Athouel",
     author_email="sathouel@gmail.com",
     description="A simple api client for Mirakl plateform",
